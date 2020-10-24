4ae47c9366fa82cb218364bb3c642ad8be38baf7
BasiaM1 <b.murzynowska@wp.pl>
2020-10-24 22:38:43 +0200

    Add RWD for Brands component

--- src/components/features/Brands/BrandsContainer.js
+++ src/components/features/Brands/BrandsContainer.js
@@ -1,9 +1,11 @@
 import Brands from './Brands';
 import { connect } from 'react-redux';
 import { getAllBrands } from '../../../redux/brandsRedux';
+import { getViewport } from '../../../redux/viewportRedux';
 
 const mapStateToProps = state => ({
   brands: getAllBrands(state),
+  viewport: getViewport(state),
 });
 
 export default connect(mapStateToProps)(Brands);
