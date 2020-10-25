1367bc021fae5502a686305314facad0504fc2f9
tomek <tomek@tomekkonieczny.com>
2020-10-25 20:42:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import store, { persistor } from './redux/store';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './styles/bootstrap.scss';
 import './styles/global.scss';
-
 import MainLayout from './components/layout/MainLayout/MainLayout';
+import BlogArticle from './components/views/BlogArticle/BlogArticle';
 import Homepage from './components/views/Homepage/Homepage';
+import NotFound from './components/views/PageNotFound/PageNotFound';
 import ProductList from './components/views/ProductList/ProductList';
 import ProductPage from './components/views/ProductPage/ProductPageContainer';
-import NotFound from './components/views/PageNotFound/PageNotFound';
-import BlogArticle from './components/views/BlogArticle/BlogArticle';
+import store, { persistor } from './redux/store';
 
 const App = () => (
   <Provider store={store}>
@@ -32,5 +30,4 @@ const App = () => (
     </PersistGate>
   </Provider>
 );
-
 export default App;
