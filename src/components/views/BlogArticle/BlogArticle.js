22090eea7efba87c6617f94c3b4bd943b00b8d8e
Marianna Sowinska <marianna.sowi@gmail.com>
2020-10-26 13:46:19 +0100

    Blog code chenges

--- src/components/views/BlogArticle/BlogArticle.js
+++ src/components/views/BlogArticle/BlogArticle.js
@@ -1,17 +1,44 @@
-//import PropTypes from 'prop-types';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 import React from 'react';
 
-import styles from './BlogArticle.module.scss';
-//import LatestBlog from '../../features/LatestBlog/LatestBlog';
+import CountComments from '../../features/CountComments/CountComments';
 
-const BlogArticle = () => {
-  return (
-    <div className={styles.root}>
-      <div className={styles.blog}>
-        <h3>Blog Article</h3>
-      </div>
+import styles from './BlogArticle.module.scss';
+
+const BlogArticle = ({ id, date, comments, title, description, image }) => (
+  <div className={styles.root}>
+    <div className={styles.photo}>
+      <img src={image} alt={id} />
     </div>
-  );
+    <div className={styles.body}>
+      <div className='row'>
+        <div className='col-12'>
+          <p>
+            <FontAwesomeIcon icon={faCalendar}></FontAwesomeIcon> {date}
+          </p>
+        </div>
+        <div className='col-6'>
+          <CountComments comments={comments} />
+        </div>
+      </div>
+      <h4>
+        <a href='/#'>{title}</a>
+      </h4>
+      <p className={styles.description}>{description}</p>
+      <BlogArticle exact path={'/blog/:blogId'} component={BlogArticle} />
+    </div>
+  </div>
+);
+
+BlogArticle.propTypes = {
+  id: PropTypes.string.isRequired,
+  date: PropTypes.string,
+  comments: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
 };
 
 export default BlogArticle;
