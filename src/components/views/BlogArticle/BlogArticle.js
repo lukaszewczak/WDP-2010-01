b40eba85d9e01f570eb939b972ef883f9cb92f86
Marianna Sowinska <marianna.sowi@gmail.com>
2020-10-27 20:03:56 +0100

    Revert "Blog code changes"
    This reverts commit 1c103337539cb4fb69f7e86150b2664a01eba6df.

--- src/components/views/BlogArticle/BlogArticle.js
+++ src/components/views/BlogArticle/BlogArticle.js
@@ -1,44 +1,11 @@
-import { faCalendar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import PropTypes from 'prop-types';
-import React from 'react';
+//import PropTypes from 'prop-types';
 
-import CountComments from '../../features/CountComments/CountComments';
+import React from 'react';
 
 import styles from './BlogArticle.module.scss';
 
-const BlogArticle = ({ id, date, comments, title, description, image }) => (
-  <div className={styles.root}>
-    <div className={styles.photo}>
-      <img src={image} alt={id} />
-    </div>
-    <div className={styles.body}>
-      <div className='row'>
-        <div className='col-12'>
-          <p>
-            <FontAwesomeIcon icon={faCalendar}></FontAwesomeIcon> {date}
-          </p>
-        </div>
-        <div className='col-6'>
-          <CountComments comments={comments} />
-        </div>
-      </div>
-      <h4>
-        <a href='/#'>{title}</a>
-      </h4>
-      <p className={styles.description}>{description}</p>
-      <BlogArticle exact path={'/blog/:blogId'} component={BlogArticle} />
-    </div>
-  </div>
-);
+const BlogArticle = () => <div className={styles.root}>This is BlogArticle</div>;
 
-BlogArticle.propTypes = {
-  id: PropTypes.string.isRequired,
-  date: PropTypes.string,
-  comments: PropTypes.string,
-  title: PropTypes.string,
-  description: PropTypes.string,
-  image: PropTypes.string,
-};
+BlogArticle.propTypes = {};
 
 export default BlogArticle;
